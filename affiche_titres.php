ffd6276404c90d4e70cbd8930ab73ba58e4bdb1b
13floyd13 <patricemartin38@gmail.com>
2021-04-12 22:13:28 +0200

    Classe Nouvelles + Classe flux+ classe fluxDAO

--- affiche_titres.php
+++ affiche_titres.php
@@ -1,14 +1,3 @@
 <?php
-require_once fluxDAO_class.php;
-/*
-function afficherTitres($url)
-    {
-        $sql= "SELECT titre, id , flux
-                FROM flux, nouvelles
-                WHERE $url =url AND flux.url=nouvelles.flux
-                ORDER BY id";
-        $flux= $this->db->query($sql);
-        $result=$flux->fetchall(PDO::FETCH_CLASS);
-        return $result[0];
 
-    }
+
---
+++ nouvelles_class.php
@@ -0,0 +1,79 @@
+<?php
+
+
+class nouvelles_class
+{
+//attributs
+private int $id;
+private string $date;
+private string $titre;
+private string $description;
+private string $lien;
+private string $image;
+private string $flux;
+
+//constructeurs
+public function __construct(int $id,string $date, string $titre, string $description, string $lien, string $image, string $flux)
+{
+    $this->id=$id;
+    $this->date=$date;
+    $this->titre=$titre;
+    $this->description=$description;
+    $this->lien=$lien;
+    $this->image=$image;
+    $this->flux=$flux;
+}
+//méthodes
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDate(): string
+    {
+        return $this->date;
+    }
+    /**
+     * @return string
+     */
+    public function getTitre(): string
+    {
+        return $this->titre;
+    }
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+    /**
+     * @return string
+     */
+    public function getLien(): string
+    {
+        return $this->lien;
+    }
+    /**
+     * @return string
+     */
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+    /**
+     * @return string
+     */
+    public function getFlux(): string
+    {
+        return $this->flux;
+    }
+
+
+}
