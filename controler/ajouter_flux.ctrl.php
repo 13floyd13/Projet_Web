b6117c429a51e470bac3b8b842ec97fa649289c6
Chiogros <alexandre.bouijoux@gmail.com>
2021-04-13 16:01:16 +0200

    ajouter_flux.ctrl.php : corrections

--- controler/ajouter_flux.ctrl.php
+++ controler/ajouter_flux.ctrl.php
@@ -3,27 +3,29 @@ require_once("../view/ajouter_flux.view.php");
 
 $i_url = $_POST['i_url'];
 $i_nom_flux = $_POST['i_nom_flux'];
-
-if (!isset($i_url) || !isset($i_nom_flux)) {
-    return ;
-}
-
 $flux_db = new FluxDAO();
 $fluxUtilisateur_db = new Flux_utilisateurDAO();
-
 $fluxUtilisateur_url = $fluxUtilisateur_db->getURL($i_nom_flux);
+$fluxUtilisateur_nom = $fluxUtilisateur_db->getNom($i_url);
+$flux = $flux_db->get($i_url);
+
+if (!isset($i_url)) {
+    return ;
+}
+if (!isset($i_nom_flux) || (isset($i_nom_flux) && empty($i_nom_flux))) {
+    $i_nom_flux = simplexml_load_file($i_url)->channel->title;
+}
+
 if ($fluxUtilisateur_url) {
     // message d'erreur "Ce flux est déjà dans votre liste"
 }
 
-$fluxUtilisateur_nom = $fluxUtilisateur_db->getNom($i_url);
+
 if ($fluxUtilisateur_nom) {
     // message d'erreur "Ce nom est déjà utilisé pour un autre flux"
 }
 
 // le flux n'existe pas dans la liste de l'utilisateur
-
-$flux = $flux_db->get($i_url);
 if ($flux == null) {
     // on ajoute ce flux dans la table des flux
     $flux_db->addFlux($i_url);
