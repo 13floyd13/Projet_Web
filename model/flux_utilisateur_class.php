e7706bf1922c8f35413e5f82e1fef8bc8aac6087
13floyd13 <patricemartin38@gmail.com>
2021-04-13 15:58:16 +0200

    flux_utilisateur et DAO

---
+++ model/flux_utilisateur_class.php
@@ -0,0 +1,67 @@
+<?php
+
+
+class Flux_utilisateur
+{
+//attributs
+private string $flux;
+private string $login;
+private string $nom;
+private string $categorie;
+
+//constructeur
+    public function __construct(string $flux, string $login, string $nom,string $categorie){
+        $this->flux=$flux;
+        $this->login=$login;
+        $this->nom=$nom;
+        $this->categorie=$categorie;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFlux(): string
+    {
+        return $this->flux;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCategorie(): string
+    {
+        return $this->categorie;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $categorie
+     */
+    public function setCategorie(string $categorie): void
+    {
+        $this->categorie = $categorie;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+}
