3d1664595c09d67b5ee1560663417a3f240bcdbc
Chiogros <alexandre.bouijoux@gmail.com>
2021-04-13 13:00:27 +0200

    js + php pour écrire dans $_SESSION si les cases se cochent

---
+++ controler/infos_a_afficher.js
@@ -0,0 +1,20 @@
+document.getElementById("opt_titre").onclick = function() {
+    isChecked(this);
+}
+
+document.getElementById("opt_image").onclick = function() {
+    isChecked(this);
+}
+
+document.getElementById("opt_description").onclick = function() {
+    isChecked(this);
+}
+
+document.getElementById("opt_lien").onclick = function() {
+    isChecked(this);
+}
+
+function isChecked(checkbox) {
+    let valeur = checkbox.checked;
+    $.post("session_setter.php", {"id_bouton": checkbox.id, "check": valeur});
+}
