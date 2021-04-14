31961338b85c8e1f03f6a287a9c92e9ae6f3ce50
GARAND Léo <garandl@hostname.iut2.upmf-grenoble.fr>
2021-04-14 10:33:02 +0200

    session1

--- controler/login.ctrl.php
+++ controler/login.ctrl.php
@@ -4,7 +4,6 @@ require('../model/utilisateurDAO_class.php');
 
 if (isset($_POST['lg'])) {
   $login = $_POST['lg'];
-  $_SESSION['login'] = $login;
 } else {
   $login = "";
 }
@@ -20,6 +19,7 @@ if ($login === "" || $mdp === "" || !$dao->isExistUtilisateur($login)) {
   require('../view/login.view.html');
 } else {
   if (strcmp($mdp, $dao->getUtilisateur($login)->getMp())==0) {
+    $_SESSION['login'] = $login;
     require('../view/main.view.php');
   } else {
     require('../view/login.view.html');
