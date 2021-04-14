4b18afea6e8ff8b3dd1350891a8685ae886b6868
13floyd13 <patricemartin38@gmail.com>
2021-04-14 16:14:39 +0200

    selections_nouvelles

---
+++ controler/selection_nouvelles.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$nomFlux= htmlentities($_GET['flux']);
+$flux_utilisateur_db = new Flux_utilisateurDAO();
+$flux=$flux_utilisateur_db->getFlux_utilisateur($nomFlux,$_SESSION["login"])->getFlux();
+$nouvelles_bd= new NouvellesDAO();
+$nouvelles=$nouvelles_bd->getNouvellesParFlux($flux);
+require'afficher_nouvelles.ctrl.php';
+
+
+
+
+
