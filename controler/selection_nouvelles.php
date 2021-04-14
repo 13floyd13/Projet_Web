70921d6331ca8d178291c487076837d03ebea6f1
13floyd13 <patricemartin38@gmail.com>
2021-04-14 17:26:48 +0200

    voila

--- controler/selection_nouvelles.php
+++ controler/selection_nouvelles.php
@@ -1,11 +1,22 @@
 <?php
 session_start();
-$nomFlux= htmlentities($_GET['flux']);
 $flux_utilisateur_db = new Flux_utilisateurDAO();
-$flux=$flux_utilisateur_db->getFlux_utilisateur($nomFlux,$_SESSION["login"])->getFlux();
-$nouvelles_bd= new NouvellesDAO();
-$nouvelles=$nouvelles_bd->getNouvellesParFlux($flux);
-require'afficher_nouvelles.ctrl.php';
+$nouvelles_bd = new NouvellesDAO();
+if ($nomFlux= htmlentities($_GET['flux']) !== null){
+    $flux = $flux_utilisateur_db->getFlux_utilisateur($nomFlux, $_SESSION["login"])->getFlux();
+    $nouvelles_bd = new NouvellesDAO();
+    $nouvelles = $nouvelles_bd->getNouvellesParFlux($flux);
+    require'afficher_nouvelles.ctrl.php';
+}else{
+    $tab_flux=$flux_utilisateur_db->getFlux_utilisateurByLogin($_SESSION["login"]);
+    foreach ($tab_flux as $flux){
+        $nouvelles = $nouvelles_bd->getNouvellesParFlux($flux);
+        require'afficher_nouvelles.ctrl.php';
+    }
+}
+
+
+
 
 
 
