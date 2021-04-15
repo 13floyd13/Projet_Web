8aa56b086a53d73807b37bee77d59e83df177896
Chiogros <alexandre.bouijoux@gmail.com>
2021-04-15 07:49:12 +0200

    manque une accolade dans DAO

--- controler/ajouter_flux.ctrl.php
+++ controler/ajouter_flux.ctrl.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../model/fluxDAO_class.php");
 require_once("../model/flux_utilisateurDAO_class.php");
-require_once("../view/ajouter_flux.view.php");
+require_once("../controler/actus.ctrl.php");
 
 session_start();
 
