a74d5b9171da923d7b06dea9960af5ac50c6c6e4
Leo-GRD <leo.garand@outlook.fr>
2021-04-15 19:28:41 +0200

    fonctionnel ma gueule !

--- controler/actualisation_flux.php
+++ controler/actualisation_flux.php
@@ -46,9 +46,8 @@
         }
     }
 
-// if (!function_exists('str_contains')) {
+ if (!function_exists('str_contains')) {
     function str_contains($haystack, $needle) {
         return $needle !== '' && mb_strpos($haystack, $needle) !== false;
     }
-// }
-
+ }
