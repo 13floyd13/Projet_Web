3af8605e59959b0453c1b8af4b6d2d18661d6320
Chiogros <alexandre.bouijoux@gmail.com>
2021-04-15 15:55:00 +0200

    enlevé les print

--- view/actus.view.php
+++ view/actus.view.php
@@ -5,26 +5,20 @@
     <title>Main page</title>
     <?php
     if (isset($_POST['i_url']) && !empty($_POST['i_url'])) {
-        print "1";
         require_once("../controler/ajouter_flux.ctrl.php");
     }
     if (isset($_POST['url_a_supprimer']) && !empty($_POST['url_a_supprimer'])) {
-        print "2";
         require_once("../controler/supprimer_flux_utilisateur.ctrl.php");
     }
     if (isset($_GET['account']) && $_GET['account'] === "page") {
-        print "3";
         require_once ("../controler/account.ctrl.php");
     } else if (isset($erreur_nom_flux) && $erreur_nom_flux == true) {
-        print "4";
         include("../view/nav.view.php");
         include("../controler/selection_nouvelles.ctrl.php");
     } else if (isset($erreur_url_flux) && $erreur_url_flux == true) {
-        print "5";
         require_once("../view/nav.view.php");
         include("../controler/selection_nouvelles.ctrl.php");
     } else {
-        print "6";
         require_once("../view/nav.view.php");
         require_once("../controler/selection_nouvelles.ctrl.php");
     }
