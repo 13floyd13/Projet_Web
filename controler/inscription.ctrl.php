91fd91a6c1ab89b1b7459666ffa41ad96d903c38
13floyd13 <patricemartin38@gmail.com>
2021-04-15 22:40:30 +0200

    Controle Login ok

--- controler/inscription.ctrl.php
+++ controler/inscription.ctrl.php
@@ -1,25 +1,30 @@
 <?php
 require_once('../model/utilisateurDAO_class.php');
 if (isset($_POST['lg'])) {
-    $login = $_POST['lg'];
-
+    $login = htmlspecialchars($_POST['lg']);
 } else {
     $login = "";
 }
+
 if (isset($_POST['mdp'])) {
     $mdp = password_hash($_POST['mdp'],PASSWORD_DEFAULT,['cost' =>14]);//$_POST['mdp'];
 } else {
     $mdp = "";
 }
-$dao = new UtilisateurDAO;
-if ($login === "" || $mdp === "" || ($dao->isExistUtilisateur($login))) {
-    print ("Erreur à l\'inscription, vous allez être redirigé vers la page connexion");
-    sleep(10);
+if (!ctype_alnum($login)){
     require('../view/login.view.html');
-}else{
-    $utilisateur= new Utilisateur($login,$mdp);
-    $dao->addUtilisateur($utilisateur);
-    print ("Inscription réussie, vous allez être redirigé vers la page connexion");
-    sleep(10);
-    require('../view/login.view.html');
+    print "login incorrect";
+    var_dump(!ctype_alnum($login));
+}
+else {
+    $dao = new UtilisateurDAO;
+    if ($login === "" || $mdp === "" || ($dao->isExistUtilisateur($login))) {
+        print ("Erreur à l\'inscription, vous allez être redirigé vers la page connexion");
+        require('../view/login.view.html');
+    } else {
+        $utilisateur = new Utilisateur($login, $mdp);
+        $dao->addUtilisateur($utilisateur);
+        print ("Inscription réussie, vous allez être redirigé vers la page connexion");
+        require_once('../view/login.view.html');
+    }
 }
