1b3a5e9f4e351331b32362729600dbd07cb07ae3
Chiogros <32033845+Chiogros@users.noreply.github.com>
2021-04-15 11:13:00 +0200

    Add files via upload

---
+++ model/flux_utilisateurDAO_class.php
@@ -0,0 +1,161 @@
+<?php
+include(dirname(__FILE__).'/flux_utilisateur_class.php');
+
+
+class Flux_utilisateurDAO
+{
+
+    private PDO $db;
+
+    function __construct()
+    {
+        $database = 'sqlite:' . dirname(__FILE__) . '/../data/newsDB'; // Data source name
+        try {
+            $this->db = new PDO($database);
+        } catch (PDOException $e) {
+            die ("Erreur : " . $e->getMessage());
+        }
+    }
+
+    function getNomFlux_utilisateur(string $flux, string $login): Flux_utilisateur
+    {
+        $login = $this->db->quote($login);
+        $commandeRequete = "SELECT nom FROM flux_utilisateur WHERE flux= :flux AND login=:login";
+        $requete = $this->db->prepare($commandeRequete);
+        $requete->bindParam('flux',$flux,PDO::PARAM_STR );
+        $requete->bindParam('login',$login,PDO::PARAM_STR);
+        if ($requete) {
+            $requete->execute();
+            $resultat = $requete->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Flux_utilisateur");
+            return $resultat[0];
+        }
+    }
+
+    function getFlux_utilisateur(string $nom, string $login): Flux_utilisateur
+    {
+        $login = $this->db->quote($login);
+        $nom = $this->db->quote($nom);
+        $commandeRequete = "SELECT * FROM flux_utilisateur WHERE nom= :nom AND login= :login";
+        $requete = $this->db->prepare($commandeRequete);
+        $requete->bindParam('nom',$nom,PDO::PARAM_STR );
+        $requete->bindParam('login',$login,PDO::PARAM_STR);
+        if ($requete) {
+            $requete->execute();
+            $resultat = $requete->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Flux_utilisateur");
+            return $resultat[0];
+        }
+    }
+
+    function getFlux_utilisateurByLogin(string $login): array
+    {
+        $login = $this->db->quote($login);
+        $commandeRequete = "SELECT * FROM flux_utilisateur WHERE login= :login";
+        $requete = $this->db->prepare($commandeRequete);
+        $requete->bindParam('login',$login,PDO::PARAM_STR);
+        if ($requete) {
+            $requete->execute();
+            $resultat = $requete->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Flux_utilisateur");
+            return $resultat;
+        }
+    }
+
+    function getNombreFlux_utilisateur(): int
+    {
+        $commandeRequete = "SELECT COUNT(Distinct flux) FROM flux_utilisateurs";
+        $requete = $this->db->prepare($commandeRequete);
+        if ($requete) {
+            $requete->execute();
+            $resultat = $requete->fetch()['flux'];
+            return $resultat;
+        }
+    }
+
+    function isExistFlux_utilisateur(string $login, string $flux): bool
+    {
+        $login = $this->db->quote($login);
+        $commandeRequete = "SELECT * FROM nouvelles WHERE flux= :flux AND login= :login";
+        $requete = $this->db->prepare($commandeRequete);
+        $requete->bindParam('flux',$flux,PDO::PARAM_STR );
+        $requete->bindParam('login',$login,PDO::PARAM_STR);
+        if ($requete) {
+            $requete->execute();
+            $resultat = $requete->fetchAll();
+            $requete->closeCursor();
+            return count($resultat) > 0;
+        }
+        return false;
+    }
+
+    function addFlux_utilisateur(Flux_utilisateur $flux_utilisateur)
+    {
+        if ($this->isExistFlux_utilisateur($flux_utilisateur->getLogin(), $flux_utilisateur->getFlux())) {
+            return;
+        }
+        $login = $this->db->quote($flux_utilisateur->getLogin());
+        $nom = $this->db->quote($flux_utilisateur->getNom());
+        $categorie = $this->db->quote($flux_utilisateur->getCategorie());
+        $flux= $flux_utilisateur->getFlux();
+        //$commandeRequete = 'INSERT INTO flux_utilisateur(flux, login, nom, categorie) VALUES(\'' . $flux_utilisateur->getFlux() . '\', ' . $login . ', ' . $nom . ', ' . $categorie . ')';
+        $commandeRequete= 'INSERT INTO flux_utilisateur(flux, login, nom, categorie) VALUES( :flux , :login, :nom, :categorie)';
+        $requete = $this->db->prepare($commandeRequete);
+        $requete->bindParam('flux',$flux,PDO::PARAM_STR );
+        $requete->bindParam('login',$login,PDO::PARAM_STR);
+        $requete->bindParam('nom',$nom,PDO::PARAM_STR);
+        $requete->bindParam('categorie',$categorie,PDO::PARAM_STR);
+        if ($requete) {
+            $requete->execute();
+            $requete->closeCursor();
+        }
+    }
+
+    function removeFlux_utilisateur(Flux_utilisateur $flux_utilisateur)
+    {
+        if ($this->isExistFlux_utilisateur()) {
+            $fluxAdelete = $flux_utilisateur->getFlux();
+            $commandeRequete = "DELETE  FROM flux WHERE flux= :fluxAdelete";
+            $requete = $this->db->prepare($commandeRequete);
+            $requete->bindParam('fluxAdelete',$fluxAdelete,PDO::PARAM_STR);
+            if ($requete) {
+                $requete->execute();
+                $requete->closeCursor();
+            }
+        }
+    }
+    function removeFlux_utilisateurByFlux($flux,$login){
+        if ($this->isExistFlux_utilisateur()) {
+            $commandeRequete = "DELETE  FROM flux WHERE flux= :flux AND login= :login";
+            $requete = $this->db->prepare($commandeRequete);
+            $requete->bindParam('flux',$flux,PDO::PARAM_STR );
+            $requete->bindParam('login',$login,PDO::PARAM_STR);
+            if ($requete) {
+                $requete->execute();
+                $requete->closeCursor();
+            }
+        }
+    }
+
+    function getCategories($login): array
+    {
+        $login = $this->db->quote($login);
+        $commandeRequete = "SELECT DISTINCT categorie FROM flux_utilisateur WHERE login= :login ORDER BY categorie";
+        $requete = $this->db->prepare($commandeRequete);
+        $requete->bindParam('login',$login,PDO::PARAM_STR);
+        if ($requete) {
+            $requete->execute();
+            $resultat = $requete->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Flux_utilisateur");
+            return $resultat;
+        }
+    }
+    function getFlux_utilisateurByCategories($login,$categorie): array {
+        $login = $this->db->quote($login);
+        $commandeRequete = "SELECT DISTINCT flux FROM flux_utilisateur WHERE login= :login AND categorie= :categorie";
+        $requete = $this->db->prepare($commandeRequete);
+        $requete->bindParam('login',$login,PDO::PARAM_STR);
+        $requete->bindParam('categorie',$categorie,PDO::PARAM_STR);
+        if ($requete) {
+            $requete->execute();
+            $resultat = $requete->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Flux_utilisateur");
+            return $resultat;
+        }
+    }
+}
---
+++ model/nouvellesDAO_class.php
@@ -0,0 +1,162 @@
+<?php
+require_once("nouvelles_class.php");
+
+class NouvellesDAO
+{
+    //attributs
+    private PDO $db;
+
+    //constructeurs
+    function __construct()
+    {
+        $database = 'sqlite:' . dirname(__FILE__) . '/../data/newsDB'; // Data source name
+        try {
+            $this->db = new PDO($database);
+        } catch (PDOException $e) {
+            die ("Erreur : " . $e->getMessage());
+        }
+    }
+
+    function getNouvelle(string $titre,string $description): Nouvelle{
+        $titre= $this->db->quote($titre);
+        $description=$this->db->quote($description);
+        $commandeRequete="SELECT * FROM nouvelles WHERE titre= :titre AND description= :description";
+        $requete = $this->db->prepare($commandeRequete);
+        $requete->bindParam('titre',$titre,PDO::PARAM_STR );
+        $requete->bindParam('description',$description,PDO::PARAM_STR);
+        if($requete) {
+            $requete->execute();
+            $resultat = $requete->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,"Nouvelle");
+            return $resultat[0];
+        }
+    }
+
+    function getNouvelles(): array {
+        $commandeRequete="SELECT * FROM nouvelles ORDER BY date ";
+        $requete=$this->db->prepare($commandeRequete);
+        if ($requete){
+            $requete->execute();
+            $resultat= $requete->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,"Nouvelle");
+            return $resultat;
+        }
+    }
+
+    function getNouvellesParFlux(string $flux): array {
+        $commandeRequete="SELECT * FROM nouvelles WHERE flux= :flux ORDER BY date";
+        $requete=$this->db->prepare($commandeRequete);
+        $requete->bindParam('flux',$flux,PDO::PARAM_STR);
+        if ($requete){
+            $requete->execute();
+            $resultat= $requete->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Nouvelle");
+            return $resultat;
+        }
+    }
+
+    function getNombreNouvelles(): int {
+        $commandeRequete = "SELECT COUNT(Distinct id) FROM nouvelles";
+        $requete= $this->db->prepare($commandeRequete);
+        if ($requete) {
+            $requete->execute();
+            $resultat= $requete->fetch();
+            return $resultat[0];
+        }
+    }
+
+    function isExistNouvelle(string $titre,string $description): bool {
+        $titre= $this->db->quote($titre);
+        $description=$this->db->quote($description);
+        $commandeRequete= "SELECT id FROM nouvelles WHERE description = :description AND titre = :titre";
+        $requete= $this->db->prepare($commandeRequete);
+        $requete->bindParam('titre',$titre,PDO::PARAM_STR );
+        $requete->bindParam('description',$description,PDO::PARAM_STR);
+        if($requete){
+            $requete->execute();
+            $resultat=$requete->fetchAll();
+            $requete->closeCursor();
+            return count($resultat) >0;
+        }
+        return false;
+    }
+
+    /*function addNouvelle(SimpleXMLElement $nouvelle, Flux $flux){
+        if ($this->isExistNouvelle($nouvelle->title, $nouvelle->description)){
+            return;
+        }
+        $url_image = $this->getURLImage($nouvelle);
+        $img = "";
+        if ($url_image != "")
+            $img = '../data/images/image' . $this->getNombreNouvelles() . '.' . $this->extensionImage($url_image);
+
+        $titre = $this->db->quote($nouvelle->title);
+        $description = $this->db->quote($nouvelle->description);
+        $pubDate = strftime("%Y-%m-%d %H:%M:%S", strtotime($nouvelle->pubDate));
+        $flux = $this->db->quote($flux->getUrl());
+
+        $commandeRequete = 'INSERT INTO nouvelles(date, titre, description, lien, image, flux) VALUES(\'' . $pubDate . '\', ' . $titre . ', ' . $description . ', \'' . $nouvelle->link . '\', \'' . $img . '\', ' . $flux . ')';
+        $requete = $this->db->prepare($commandeRequete);
+        if ($requete) {
+            $requete->execute();
+            $requete->closeCursor();
+        }
+    }*/
+
+    function addNouvelle(Nouvelle $nouvelle){
+        if ($this->isExistNouvelle($nouvelle->getTitre(), $nouvelle->getDescription())){
+            return;
+        }
+        $img = $nouvelle->getImage();
+        $titre = $this->db->quote($nouvelle->getTitre());
+        $description = $this->db->quote($nouvelle->getDescription());
+        $pubDate = strftime("%Y-%m-%d %H:%M:%S", strtotime($nouvelle->getDate()));
+        $flux = $this->db->quote($nouvelle->getFlux());
+
+        $commandeRequete = 'INSERT INTO nouvelles(date, titre, description, lien, image, flux) VALUES(\'' . $pubDate . '\', ' . $titre . ', ' . $description . ', \'' . $nouvelle->getLien() . '\', \'' . $img . '\', ' . $flux . ')';
+        $requete = $this->db->prepare($commandeRequete);
+        if ($requete) {
+            $requete->execute();
+            $requete->closeCursor();
+        }
+    }
+
+    function removeNouvelle(Nouvelle $nouvelle){
+        if ($this->isExistNouvelle($nouvelle->getTitre(), $nouvelle->getDescription())){
+            $idAdelete= $nouvelle->getId();
+            $commandeRequete="DELETE FROM flux WHERE id=$idAdelete";
+            $requete= $this->db->prepare($commandeRequete);
+            if($requete){
+                $requete->execute();
+                $requete->closeCursor();
+            }
+        }
+    }
+
+    private function getURLImage($nouvelle) : string
+    {
+        if (count($nouvelle->children('media', True)) != 0) {
+            $url_image = $nouvelle->children('media', True)->content->attributes()['url'];
+        } else {
+            if (isset($nouvelle->enclosure)) {
+                $url_image = $nouvelle->enclosure->attributes()['url'];
+            }
+        }
+
+        if (isset($url_image)&&($this->extensionImage($url_image)!="")) {
+            return $url_image;
+        } else {
+            return "";
+        }
+    }
+
+    private function str_contains($haystack, $needle)
+    {
+        return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+    }
+
+    private function extensionImage($url)
+    {
+        if ($this->str_contains($url,'.jpg')||$this->str_contains($url,'.jpeg')||$this->str_contains($url,'.JPEG')||$this->str_contains($url,'.JPG')) return "jpg";
+        else if ($this->str_contains($url,'.png')||$this->str_contains($url,'.PNG')) return "png";
+        else if ($this->str_contains($url,'.gif')||$this->str_contains($url,'.GIF')) return "gif";
+        else return "";
+    }
+}
